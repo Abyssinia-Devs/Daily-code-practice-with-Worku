8d8ccf9fa7b214f9e7723957c1526134e962a38a
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-04 14:52:58 +0300

    count the total pages and see the contents of each page brefly

--- leetcode/choose_best_3_project/script1.py
+++ leetcode/choose_best_3_project/script1.py
@@ -1,15 +1,14 @@
 import re
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader #pypdf is better but for the sack of experiment let's use PyPDF2
 
+path = r'c:/Users/hi/Downloads/oreillyBooks/The Big Book of Small Python Projects.pdf'
 
-path = r'c:\\Users\\hi\\Downloads\\oreillyBooks\\The Big Book of Small Python Projects.pdf'
+reader = PdfReader(path) #which is read only mode
 
-reader = PdfReader(path)
-
-print('pages', len(reader.pages))
+print('Total pages:', len(reader.pages))
 for i in range(min(25, len(reader.pages))):
     
     text = reader.pages[i].extract_text() or ''
     text = re.sub(r'\s+', ' ', text)
     print(f'---PAGE {i+1}---')
-    print(text[:120])
+    print(text[:1200])

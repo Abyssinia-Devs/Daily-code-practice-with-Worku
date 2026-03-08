6859df15e16cbb1f34b6c888133e2fa29ce7bf96
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-08 11:11:32 +0300

    rewrite folder struc

--- leetcode/wed.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        sort = sorted([(bin(x).count('1'),x) for x in  (arr)])
-        result = [x[1] for x in sort]
-        return result

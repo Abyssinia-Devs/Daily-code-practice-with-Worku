42a8bec9a3396242203b6a7715e2437b0e6a8fac
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-16 19:09:54 +0300

    136. Single Number

---
+++ mon_march16.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n =0;
+        for(auto x: nums)
+        {
+           n ^=x;
+        }
+        return n;
+    }
+};

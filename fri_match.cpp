77ac6ee480795e9161332d4f362ef526ad3601a8
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-20 19:20:04 +0300

    66. Plus One

---
+++ fri_match.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+            int n = digits.size();
+    // Start from the last digit and move towards the first digit
+    for (int i = n - 1; i >= 0; --i) {
+        // If the current digit is less than 9, just increment it and return
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        // If the current digit is 9, set it to 0 and continue to the next digit
+        digits[i] = 0;
+    }
+    
+    // If we reach here, it means we had a carry out of the most significant digit
+    // So we need to add a new digit at the beginning
+    digits.insert(digits.begin(), 1);
+    return digits;
+    }
+};

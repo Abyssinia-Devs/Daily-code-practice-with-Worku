79d5f78e9c5e128ae8bf002fb4e763105eebc1e0
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-21 20:21:41 +0300

    3643. Flip Square Submatrix Vertically

---
+++ sat_march_21.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+         // Flip the submatrix vertically
+        for (int i = 0; i < k / 2; ++i) {
+        for (int j = 0; j < k; ++j) {
+            swap(grid[x + i][y + j], grid[x + k - 1 - i][y + j]);
+        }
+    }
+    return grid;
+    }
+};
+
+
